678099b23faccea32f244911a15b8fd6801402a7
bae <baesj11@gmail.com>
2020-11-29 01:15:00 +0900

    cmd commit

--- src/main/java/praticefor/PraticeFirst.java
+++ src/main/java/praticefor/PraticeFirst.java
@@ -2,6 +2,7 @@ package praticefor;
 
 public class PraticeFirst {
     public static void main(String[] args) {
+        System.out.println("test git");
         for (int i = 1; i < 10; i++) {
             System.out.println("first");
         }
