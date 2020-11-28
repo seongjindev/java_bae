1e539adc1656c78b2a8797b112b8483308ab10f0
bae <baesj11@gmail.com>
2020-11-29 00:45:26 +0900

    pratice_for_star2

--- src/main/java/praticefor/PraticeFirst.java
+++ src/main/java/praticefor/PraticeFirst.java
@@ -1,2 +1,25 @@
-package praticefor;public class PraticeFirst {
+package praticefor;
+
+public class PraticeFirst {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println("first");
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("\n");
+            if(i<5){
+                for(int j = 1; j<=i;j++){
+                    System.out.print("*");
+                }
+            }else{
+                for(int k = 10; k>=i;k--){
+                    System.out.print("*");
+                }
+            }
+
+        }
+
+    }
 }
+
