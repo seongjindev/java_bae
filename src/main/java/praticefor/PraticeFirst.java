604bcf7669d47fb8ba80816190feeb930c5fc2ad
bae <baesj11@gmail.com>
2020-12-01 01:04:37 +0900

    99dan

--- src/main/java/praticefor/PraticeFirst.java
+++ src/main/java/praticefor/PraticeFirst.java
@@ -6,15 +6,20 @@ public class PraticeFirst {
         for (int i = 1; i < 10; i++) {
             System.out.println("first");
         }
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.printf("%d * %d = %d\n", i, j, i * j);
+            }
+        }
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("\n");
-            if(i<5){
-                for(int j = 1; j<=i;j++){
+            if (i < 5) {
+                for (int j = 1; j <= i; j++) {
                     System.out.print("*");
                 }
-            }else{
-                for(int k = 10; k>=i;k--){
+            } else {
+                for (int k = 10; k >= i; k--) {
                     System.out.print("*");
                 }
             }
